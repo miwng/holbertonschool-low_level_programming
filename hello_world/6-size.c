e4231fae089308d6a63e792725d00b3cd2a391a0
miwng <ng.michelle.w@gmail.com>
2025-06-19 07:05:12 +0000

    print sizes of various types on 32 and 64

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char:lu,")
+	printf("Size of an int:")
+	printf("Size of a long int:")
+	printf("Size of a long long int:")
+	printf("Size of a float:")
+
+	printf("Size of a char:lu,")
+        printf("Size of an int:")
+        printf("Size of a long int:")
+        printf("Size of a long long int:")
+        printf("Size of a float:")
+
+	return (0);
+}
