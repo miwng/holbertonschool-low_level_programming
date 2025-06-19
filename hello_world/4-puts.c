3100037c69a282aabdab8f99045a57a4eb2edebb
miwng <ng.michelle.w@gmail.com>
2025-06-19 05:55:35 +0000

    write statement with double quotes using puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main () {
-	puts (" \"Programming is like building a multilingual puzzle");
+/**
+ * main(void) - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
