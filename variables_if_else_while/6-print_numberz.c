399be638b5234d707cfe6f15915650c4741f369e
miwng <ng.michelle.w@gmail.com>
2025-06-20 06:37:57 +0000

    putchar single digits of base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 48; d < 58; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
