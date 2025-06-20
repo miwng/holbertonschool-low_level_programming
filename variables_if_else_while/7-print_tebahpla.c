30732d816091608157a8c2201311227f000ba1e7
miwng <ng.michelle.w@gmail.com>
2025-06-20 06:41:39 +0000

    putchar alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 122; d > 96; d--)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
