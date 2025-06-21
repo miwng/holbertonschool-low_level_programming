513b49336678863e53620127a984d232eb059224
miwng <ng.michelle.w@gmail.com>
2025-06-21 11:19:01 +0000

    _putchar for consecutive minutes from 00:00 to 23:59 with for loop

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: Checks if lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
