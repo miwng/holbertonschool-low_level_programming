c61e1775f2a9a533b0e4570515c91ad3a5359efe
miwng <ng.michelle.w@gmail.com>
2025-06-21 13:36:28 +0000

    print numbers from n to 98 sequentially

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,25 +12,20 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n <= 98)
 	{
-		printf("98 \n");
-	}
-	else if (n < 98)
-	{
-		for (n = n; n < 98; n++)
+		for (n = n; n <= 97; n++)
 		{
 			printf("%d, ", n);
 		}
-		printf("98 \n");
+		printf("98\n");
 	}
 	else
 	{
-		for (n = n; n > 97; n--)
+		for (n = n; n >= 98; n--)
 		{
 			printf("%d, ", n);
 		}
-		printf("98 \n");
 	}
 }
 
