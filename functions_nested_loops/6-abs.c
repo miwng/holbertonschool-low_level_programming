84914e3b402020ef031d52184be0759b5fb7550b
miwng <ng.michelle.w@gmail.com>
2025-06-21 10:39:49 +0000

    absolute value (abs) with if else loop

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Entry point
+ * @n: Variable
+ *
+ * Description: Checks if lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
