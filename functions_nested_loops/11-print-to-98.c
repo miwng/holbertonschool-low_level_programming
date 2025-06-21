8a80b6d61f3e443e890df9906425793816997e78
miwng <ng.michelle.w@gmail.com>
2025-06-21 13:19:56 +0000

    print numbers from n to 98 sequentially

--- functions_nested_loops/11-print-to-98.c
+++ functions_nested_loops/11-print-to-98.c
@@ -1,16 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * add- Entry point
+ * print_to_98- Entry point
  *
- * Description: Checks if lowercase
+ * @n: Starting number
  *
  * Return: Always 0 (Success)
  */
 
-int add(int, int)
+void print_to_98(int n)
 {
-
-
+	if (n == 98)
+	{
+		printf("98 '\n");
+	}
+	else if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("98 \n");
+	}
+	else
+	{
+		for (n = n; n > 97; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("98 \n");
+	}
 }
+
