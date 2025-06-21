f362e32d8ad0ad8e5ebcd6f9b934b1a048d10415
miwng <ng.michelle.w@gmail.com>
2025-06-21 08:38:29 +0000

    amended for betty style coding

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
+ *
+ * Description: Prints alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
@@ -11,9 +13,9 @@ void print_alphabet(void)
 {
 	char p;
 
-        for (p = 97; p < 123; p++)
-        {
+	for (p = 97; p < 123; p++)
+	{
 		_putchar(p);
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
