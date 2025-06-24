0bc3d0dc9d3d2e082a675230f85cbd5ebcd93c1b
miwng <ng.michelle.w@gmail.com>
2025-06-24 07:22:26 +0000

    if else loop for upper case char

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @c: character to check for upper case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char p);
+int _isupper(int c);
+
+#endif 
