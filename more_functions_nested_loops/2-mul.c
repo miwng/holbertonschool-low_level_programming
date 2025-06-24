58e9f0d66ca408fe5935c2df2fa6fc5c66ff6888
miwng <ng.michelle.w@gmail.com>
2025-06-24 07:48:31 +0000

    function to multiple 2 integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ *
+ * @a: character to check
+ *
+ * @b: character to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b); 
+}
