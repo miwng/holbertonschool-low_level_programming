5d22531f55740113a1a51bd8413c138732fca6ce
miwng <ng.michelle.w@gmail.com>
2025-06-25 00:37:16 +0000

    _putchar with for if loop to print numbers except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		if (numbers != 2 && numbers != 4)
+			_putchar((numbers % 10) + '0');
+	}
+	_putchar('\n');
+}
