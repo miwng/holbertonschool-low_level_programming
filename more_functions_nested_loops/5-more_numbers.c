751e6c87d7a9c68156a085a21f30b0fc43e7ab5a
miwng <ng.michelle.w@gmail.com>
2025-06-25 01:12:13 +0000

    _putchar, for and if loop to display 0 to 14 10x

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int numbers;
+	int times;
+
+	for (times = 0; times < 11; times++)
+	{
+		for (numbers = 0; numbers < 15; numbers++)
+		{
+			if (numbers > 9)
+				_putchar((numbers / 10) + '0');
+			_putchar((numbers % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
