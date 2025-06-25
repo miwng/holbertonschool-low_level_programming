92465e67cf8397835d4a55753caba565f90b8c03
miwng <ng.michelle.w@gmail.com>
2025-06-25 03:53:45 +0000

    using for loops to print # into square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - Entry point
+ *
+ * @size: the size of the square
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int height;
+	int width;
+
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			_putchar('#');
+
+			for (width = 0; width < size; width++)
+				_putchar('#');
+			if (height == size -1)
+				continue;
+			_putchar('\n');
+		}
+
+	}
+	_putchar('\n');
+}
