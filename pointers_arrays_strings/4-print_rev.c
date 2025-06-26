371cee03b5969276f8ef12e292dbef2cd9210535
miwng <ng.michelle.w@gmail.com>
2025-06-26 12:20:01 +0000

    reverse string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Entry point
+ *
+ * @s: pointer to char s
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	int index;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
