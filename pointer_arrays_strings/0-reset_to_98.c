81e9900829e01b2e8404ccd2c92cc33c4024dcda
miwng <ng.michelle.w@gmail.com>
2025-06-26 09:49:53 +0000

    pointer to int and change value to 98

---
+++ pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * *n: pointer to value of n
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
