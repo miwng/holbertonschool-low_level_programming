1ca95cfdd6421750076081271793f282426a47e5
miwng <ng.michelle.w@gmail.com>
2025-06-27 08:10:59 +0000

    function that copies string pointed to by src

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: points to buffer
+ *
+ * @src: points to string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	return (dest);
+}
