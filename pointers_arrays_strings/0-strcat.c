3d44029d7a221fd6302035171fa011daaad37216
miwng <ng.michelle.w@gmail.com>
2025-06-27 13:01:58 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Entry point
+ *
+ * @dest: pointer to value of n
+ *
+ * @src:
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	*n = 98;
+}
