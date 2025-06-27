2b37e321b6d015fa0743dabffb64d00f786490ad
miwng <ng.michelle.w@gmail.com>
2025-06-27 05:05:16 +0000

    print in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,18 +11,14 @@
 
 void print_rev(char *s)
 {
-	int length;
-	int reverse;
-	int temp;
+	int length = 0;
 
-	while(*s)
-		_putchar(*s++);
-	
-	temp = *s
+	while (s[length] != '\0')
+		length++;
 
-	for (reverse = temp - 1; reverse >= 0; reverse--)
+	for (length -= 1; length >= 0; length--)
 	{
-		_putchar(s[reverse]);	
+		_putchar(s[length]);	
 	}
 	_putchar('\n');
 }
