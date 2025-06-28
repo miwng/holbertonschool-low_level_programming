48bcea9aa0f11cf5daa2e5fc0e15cc55103977a2
miwng <ng.michelle.w@gmail.com>
2025-06-28 11:24:52 +0000

    concaternate two strings but stopping at inputted n bytes

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - Entry point
+ *
+ * @dest: pointer to dest string
+ *
+ * @src: src string to add to end of dest string
+ *
+ * @n: prevents src from null-termination if n or more bytes exist
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int dest_length = 0;
+
+	while (dest[count++])
+		dest_length++;
+
+	for (count = 0; src[count] && count < n; count++)
+		dest[dest_length++] = src[count];
+	return (dest);
+}
