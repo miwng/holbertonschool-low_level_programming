1b82fa937e809bb9a90ecf6b74624fb9354225f3
miwng <ng.michelle.w@gmail.com>
2025-07-02 04:58:15 +0000

    memcpy - copies bytes from one memory area to another

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Entry point
+ *
+ * @dest: Destination for copied bytes
+ *
+ * @src: Original location of bytes to be copied
+ *
+ * @n: Bytes of memory to be copied
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+		src[index] = dest[index];
+	return (dest);
+}
