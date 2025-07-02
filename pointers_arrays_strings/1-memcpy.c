f67d96d9dd50e7ca682c6104136e68049f8817eb
miwng <ng.michelle.w@gmail.com>
2025-07-02 05:02:28 +0000

    rearranged src to dest

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -18,6 +18,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int index;
 
 	for (index = 0; index < n; index++)
-		src[index] = dest[index];
+		dest[index] = src[index];
 	return (dest);
 }
