49d8ff1dbc7abbc5c365b693bab83986ed671f4b
miwng <ng.michelle.w@gmail.com>
2025-07-05 09:18:55 +0000

    print string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Entry point
+ *
+ * @s: prints string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
