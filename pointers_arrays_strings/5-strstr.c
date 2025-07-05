5c112709d920ad089ae970c7ea8a9c1d5c8ab578
miwng <ng.michelle.w@gmail.com>
2025-07-05 00:26:41 +0000

    full substring found within another string, then pointer to beginning of substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Entry point
+ *
+ * @haystack: String to be checked for first occurrence of substring needle
+ *
+ * @needle: Full substring to be checked in string haystack then pointer at beginning of substring
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == '\0')
+	return (haystack);
+
+	while (*haystack)
+	{
+		for (index = 0; needle[index]; index++)
+		{
+			if (haystack[index] != needle[index])
+			break;
+		}
+
+		if (needle[index] == '\0')
+		return (haystack);
+
+		haystack++;
+	}
+	return ('\0');
+}
