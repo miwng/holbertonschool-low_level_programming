ff4cab31dbf66152a73120e1e28083e2992d2add
miwng <ng.michelle.w@gmail.com>
2025-07-06 12:40:11 +0000

    arg to print new file name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argv: Array of pointers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
