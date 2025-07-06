8ac290bdb5db517db6217f508b957454b61a55b1
miwng <ng.michelle.w@gmail.com>
2025-07-06 12:49:41 +0000

    arg print number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: No of arguments
+ *
+ * @argv: Array of pointers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
