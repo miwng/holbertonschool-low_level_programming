dea4cd1b507c23a3f7d32a6d814f283d1efcd9d9
miwng <ng.michelle.w@gmail.com>
2025-07-08 02:30:51 +0000

    pointer to newly allocated space with copy of string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ *
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+	int index;
+	int size = 0;
+	char *strcopy;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		size++;
+
+	strcopy = malloc(sizeof(char) * size);
+
+	if (strcopy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		strcopy[index] = str[index];
+	return (strcopy);
+}
