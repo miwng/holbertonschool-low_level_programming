5533f8c6d2224e113d4e53731c46cbf905a4942c
miwng <ng.michelle.w@gmail.com>
2025-07-13 09:52:51 +0000

    saved in wrong directory

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog description
+ * 
+ * @name: name
+ * 
+ * @age: age
+ * 
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
