345851fe200421c4712bb7f388eed9472ed5dd94
miwng <ng.michelle.w@gmail.com>
2025-07-13 12:20:22 +0000

    free mem

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Entry point
+ *
+ * @d: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -27,5 +27,6 @@ void print_dog(struct dog *d);
 int length(char *str);
 char*_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
