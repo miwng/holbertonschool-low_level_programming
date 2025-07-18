e20ce7f64cac6a443d1457f88d0045957849f106
miwng <ng.michelle.w@gmail.com>
2025-07-18 10:22:38 +0000

    function that searches for an integer

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Entry point
+ *
+ * @array: array
+ *
+ * @size: size of array
+ *
+ * @cmp: pointer to function to action
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
