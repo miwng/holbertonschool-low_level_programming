833120a10b0966c2f4ce2cc04b3eab6fb645db2b
miwng <ng.michelle.w@gmail.com>
2025-07-20 11:58:32 +0000

    print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print string
+ *
+ * @separator: separates string
+ *
+ * @n: n
+ *
+ * @...: a variable number of parameters
+ *
+ * Return: the sum of all parameters
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list print;
+	char *str;
+	unsigned int index;
+
+	va_start(print, n);
+
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(print, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(print);
+}
