dbf3459b6686d63c7de252c5d0ad0773ca0ed3c1
miwng <ng.michelle.w@gmail.com>
2025-08-07 00:35:45 +0000

    print element of doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints elements of linked list.
+ *
+ * @h: List of nodes
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
