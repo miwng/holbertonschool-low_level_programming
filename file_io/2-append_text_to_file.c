b43428a21f2659f5df6d109822311c5b248e63b0
miwng <ng.michelle.w@gmail.com>
2025-08-08 07:37:17 +0000

    append text at end of file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - Creates a file
+ *
+ * @filename: Pointer to name
+ *
+ * @text_content: Pointer to text
+ *
+ * Return: Number of bytes
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
